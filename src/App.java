7bc0017495c7f7a62f6a02167fbc8c7e6d527dd3
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2021-12-02 06:52:28 -0300

    Update
    Update

---
+++ src/App.java
@@ -0,0 +1,20 @@
+class KeepingKidQuiet{
+
+    public static void main(String args[]){
+
+        int gumballs;
+        int kids;
+        int gumballsPerkid;
+
+        gumballs = 30;
+        kids = 4;
+        gumballPerdkid = gumballs / kids;
+
+        System.out.print("Each kid gets" );
+        System.out.print(gumballPerkid);
+        System.out.println("gumballs.");
+
+
+    }
+
+}
